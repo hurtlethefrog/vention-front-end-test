9d8ff9028da79cf2a0ad283208037011b856a9a5
Duncan H <duncan.haran@gmail.com>
2019-10-06 22:39:30 -0400

    dynamic cart assets added

--- scripts/retrieveJSON.js
+++ scripts/retrieveJSON.js
@@ -38,35 +38,55 @@ loadJSON(
     data.map(element => {
       let parentDiv = document.createElement("div");
       let newImg = document.createElement("img");
+      let imgDiv = document.createElement("div");
       let priceDiv = document.createElement("div");
       let ratingDiv = document.createElement("div");
       let titleDiv = document.createElement("div");
       let flowerInfo = document.createElement("div");
-    
-      titleDiv.className = "title"
-      priceDiv.className = "price"
+      let cartDiv = document.createElement("div");
+      let addCart = document.createElement("div");
+      let removeCart = document.createElement("div");
+
+      newImg.setAttribute("src", `${element.image}`);
+
+      titleDiv.className = "title";
+      priceDiv.className = "price";
       parentDiv.className = "flowerCard";
       newImg.className = "flowerImage";
       ratingDiv.className = "rating";
       flowerInfo.className = "flowerInfo";
-
-      newImg.setAttribute("src", `${element.image}`);
+      cartDiv.className = "cartDiv";
+      imgDiv.className = "imgDiv";
+      addCart.className = "addCart toggle";
+      removeCart.className = "removeCart toggle";
 
       let title = document.createTextNode(element.name);
       titleDiv.appendChild(title);
-      // export this
       let price = document.createTextNode(
+        // export this
         (element.price / 100).toLocaleString("en-US", {
           style: "currency",
           currency: "USD"
         })
       );
+      let inCart = document.createTextNode("In Cart");
+      let addCartText = document.createTextNode("Add to cart");
+      let removeCartText = document.createTextNode("Remove from cart");
+
+
       priceDiv.appendChild(price);
+      cartDiv.appendChild(inCart);
+      addCart.appendChild(addCartText)
+      removeCart.appendChild(removeCartText)
 
       flowerInfo.appendChild(titleDiv);
       flowerInfo.appendChild(priceDiv);
       flowerInfo.appendChild(ratingDiv);
-      parentDiv.appendChild(newImg);
+      imgDiv.appendChild(newImg);
+      imgDiv.appendChild(cartDiv);
+      imgDiv.appendChild(addCart);
+      imgDiv.appendChild(removeCart);
+      parentDiv.appendChild(imgDiv);
       parentDiv.appendChild(flowerInfo);
 
       rating(ratingDiv, element.rating);
